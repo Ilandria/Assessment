4bd8c337fd65ac26ba7f8b643e61ae49ee022446
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2022-12-10 14:06:55 -0500

    Initial Lv1 controller & Listing model
    Also included csv files.

---
+++ Assessment/Controllers/LevelOneController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assessment.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LevelOneController : ControllerBase
+	{
+	}
+}
---
+++ Assessment/Models/Listing.cs
@@ -0,0 +1,11 @@
+﻿namespace Assessment.Models
+{
+	public class Listing
+	{
+		public int Id { get; set; } = default!
+		public string ListingUrl { get; set; } = default!
+		public string Name { get; set; } = default!
+		public string Description { get; set; } = default!
+		public string PropertyType { get; set; } = default!
+	}
+}
