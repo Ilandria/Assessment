c9431956611c85133f6b4f9250571add4a9ff46a
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2022-12-10 16:42:58 -0500

    Added unit test project
    No actual tests yet, the linkage to the original "Assessment" project isn't working and no types are being seen in the test project. References look like they exist and framework versions match. Looking into it but running out of time!

---
+++ UnitTests/LevelOneControllerTests.cs
@@ -0,0 +1,23 @@
+//using Assessment.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+	// Todo: Figure out issue with linking to Assessment project. LevelOneController isn't being seen.
+	[TestClass]
+	public class LevelOneControllerTests
+	{
+		//private readonly LevelOneController controller;
+
+		public LevelOneControllerTests()
+		{
+			//controller = new LevelOneController();
+		}
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+		}
+	}
+}
