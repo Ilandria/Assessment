5871d5f7e2ca33e4b45b6d3a86198a1ac9455d90
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2022-12-10 14:33:51 -0500

    Level 1 minus unit tests complete?

--- Assessment/Controllers/LevelOneController.cs
+++ Assessment/Controllers/LevelOneController.cs
@@ -4,11 +4,14 @@ using Microsoft.VisualBasic.FileIO;
 
 namespace Assessment.Controllers
 {
+	// This controller naming is just for clarity on the test.
 	[Route("api/[controller]")]
 	[ApiController]
 	public class LevelOneController : ControllerBase
 	{
 		private const string listingsFilePath = "./CSV/listings.csv";
+
+		// Todo: Come back and add tests after level 2.
 
 		[HttpGet("~/listing")]
 		public Listing? GetListing(int id, string serverFilePath = listingsFilePath)
@@ -20,10 +23,16 @@ namespace Assessment.Controllers
 		}
 
 		[HttpGet("~/listings")]
-		public IEnumerable<Listing> GetListings(int pageNum = 1, int resultsPerPage = 2, string serverFilePath = listingsFilePath)
+		public IEnumerable<Listing> GetListings(int pageNum = 1, int resultsPerPage = 2, string? propertyType = null, string serverFilePath = listingsFilePath)
 		{
 			// Todo: Probably don't want to hard-code this here...
 			IEnumerable<Listing> listings = ReadListingsCsv(serverFilePath);
+
+			// Basic filtering by property type. This lets us use one endpoint for both cases pretty intuitively.
+			if (!string.IsNullOrWhiteSpace(propertyType))
+			{
+				listings = listings.Where(listing => string.Compare(listing.PropertyType, propertyType) == 0);
+			}
 
 			// Basic sanity limits for pagination (min 1 per page, max 10 per page, can't be on a page num below 1).
 			const int maxPageSize = 10;
@@ -33,7 +42,7 @@ namespace Assessment.Controllers
 			return listings.Skip(resultsPerPage * (pageNum - 1)).Take(resultsPerPage).ToList();
 		}
 
-		public static IEnumerable<Listing> ReadListingsCsv(string serverFilePath)
+		private static IEnumerable<Listing> ReadListingsCsv(string serverFilePath)
 		{
 			List<Listing> listings = new();
 
