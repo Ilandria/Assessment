9eeefa3da7587ab74cb62f85d39298a3a253b948
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2022-12-10 14:13:15 -0500

    Quick CSV reader, still need to parse the listings file.

--- Assessment/Controllers/LevelOneController.cs
+++ Assessment/Controllers/LevelOneController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Assessment.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.FileIO;
 
 namespace Assessment.Controllers
 {
@@ -7,5 +8,38 @@ namespace Assessment.Controllers
 	[ApiController]
 	public class LevelOneController : ControllerBase
 	{
+		public static IEnumerable<Listing> ReadListingsCsv(string serverFilePath)
+		{
+			List<Listing> listings = new();
+
+			try
+			{
+				using TextFieldParser parser = new(serverFilePath);
+
+				parser.SetDelimiters(",");
+				parser.HasFieldsEnclosedInQuotes = true;
+
+				// This assumes the file has headers. This works for now...
+				parser.ReadLine();
+
+				while (!parser.EndOfData)
+				{
+					string[]? fields = parser.ReadFields();
+
+					if (fields != null)
+					{
+						// Todo: Parse things!
+						//listings.Add();
+					}
+				}
+			}
+			catch (Exception)
+			{
+				// This is pretty hacky, but it will prevent things from crashing if the file isn't found or malformatted.
+				listings.Clear();
+			}
+
+			return listings;
+		}
 	}
 }
