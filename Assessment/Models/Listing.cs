9800b78f1a76a14a7072c0eccccd56e617375f99
Charlotte C. Brown <charlotte.clover.brown@gmail.com>
2022-12-10 14:07:33 -0500

    Forgot semicolons... :)

--- Assessment/Models/Listing.cs
+++ Assessment/Models/Listing.cs
@@ -2,10 +2,10 @@
 {
 	public class Listing
 	{
-		public int Id { get; set; } = default!
-		public string ListingUrl { get; set; } = default!
-		public string Name { get; set; } = default!
-		public string Description { get; set; } = default!
-		public string PropertyType { get; set; } = default!
+		public int Id { get; set; } = default!;
+		public string ListingUrl { get; set; } = default!;
+		public string Name { get; set; } = default!;
+		public string Description { get; set; } = default!;
+		public string PropertyType { get; set; } = default!;
 	}
 }
